893b2871b9205ad1beb75e297e022e50a3abcf59
tzmfreedom <makoto_tajitsu@hotmail.co.jp>
2016-01-10 22:40:02 +0900

    Refactor for PEP8

--- etcli/commands.py
+++ etcli/commands.py
@@ -6,9 +6,12 @@ import os
 
 import ET_Client
 
+CONFIG_PATH = '~/.fuelsdk/config.python'
+
+
 class Commands(object):
     def authenticate(self, client_id=None, client_secret=None, debug=False):
-        if client_id == None or client_secret == None:
+        if client_id is None or client_secret is None:
             self.client = ET_Client.ET_Client(debug=debug)
         else:
             self.client = ET_Client.ET_Client(
@@ -18,8 +21,11 @@ class Commands(object):
                 }, debug=debug)
 
     def configure(self, args):
-        fuelsdk_config = os.path.expanduser('~/.fuelsdk/config.python')
-        if os.path.isfile(fuelsdk_config) and raw_input('Do you want to overwrite ~/.fuelsdk/config.python?(y/n)') != 'y':
+        fuelsdk_config = os.path.expanduser(CONFIG_PATH)
+        if (
+            os.path.isfile(fuelsdk_config) and
+            raw_input('Do you want to overwrite {} ?(y/n)'.format(CONFIG_PATH)) != 'y'
+        ):
             return
 
         client_id = raw_input('Input Your ExactTarget Client ID: ')
@@ -36,7 +42,6 @@ clientid: {0}
 clientsecret: {1}
 defaultwsdl: https://webservice.exacttarget.com/etframework.wsdl
 authenticationurl: https://auth.exacttargetapis.com/v1/requestToken?legacy=1""".format(client_id, client_secret))
-
 
     def describe_de(self, args):
         """
@@ -58,10 +63,16 @@ authenticationurl: https://auth.exacttargetapis.com/v1/requestToken?legacy=1""".
         myDEColumn = ET_Client.ET_DataExtension_Column()
         myDEColumn.auth_stub = self.client
         myDEColumn.props = de_target_fields
-        myDEColumn.search_filter = {'Property' : 'DataExtension.CustomerKey','SimpleOperator' : 'equals','Value' : args.customer_key}
+        myDEColumn.search_filter = {
+            'Property': 'DataExtension.CustomerKey',
+            'SimpleOperator': 'equals',
+            'Value': args.customer_key
+        }
         response = myDEColumn.get()
-        return [self.convert_field_to_dict(result, de_target_fields) for result in response.results]
-
+        return [
+            self.convert_field_to_dict(result, de_target_fields)
+            for result in response.results
+        ]
 
     def convert_field_to_dict(self, field, target_fields):
         converted_dict = {}
@@ -69,7 +80,6 @@ authenticationurl: https://auth.exacttargetapis.com/v1/requestToken?legacy=1""".
             if hasattr(field, field_name):
                 converted_dict[field_name] = getattr(field, field_name)
         return converted_dict
-
 
     def retrieve_de(self, args):
         """
@@ -84,7 +94,8 @@ authenticationurl: https://auth.exacttargetapis.com/v1/requestToken?legacy=1""".
         row.CustomerKey = args.customer_key
         row.props = [field['Name'] for field in fields]
         response = row.get()
-        writer = csv.writer(sys.stdout, quoting=csv.QUOTE_ALL, lineterminator='\n')
+        writer = csv.writer(
+            sys.stdout, quoting=csv.QUOTE_ALL, lineterminator='\n')
         writer.writerow(row.props)
 
         for result in response.results:
@@ -95,7 +106,6 @@ authenticationurl: https://auth.exacttargetapis.com/v1/requestToken?legacy=1""".
                 else:
                     row.append(prop.Value)
             writer.writerow(row)
-
 
     def describe_all_de(self):
         """
@@ -109,12 +119,16 @@ authenticationurl: https://auth.exacttargetapis.com/v1/requestToken?legacy=1""".
         de.props = ["Name", "CustomerKey", "ObjectID"]
         response = de.get()
 
-        writer = csv.writer(sys.stdout, quoting=csv.QUOTE_ALL, lineterminator='\n')
+        writer = csv.writer(
+            sys.stdout, quoting=csv.QUOTE_ALL, lineterminator='\n')
         writer.writerow(de.props)
 
         for result in response.results:
-            writer.writerow([result.Name.encode("utf-8"), result.CustomerKey.encode("utf-8"), result.ObjectID.encode("utf-8")])
-
+            writer.writerow([
+                result.Name.encode("utf-8"),
+                result.CustomerKey.encode("utf-8"),
+                result.ObjectID.encode("utf-8")
+            ])
 
     def retrieve_subs(self):
         """
@@ -129,9 +143,13 @@ authenticationurl: https://auth.exacttargetapis.com/v1/requestToken?legacy=1""".
 
         attributes = []
         if (hasattr(response.results[0], 'Attributes')):
-            attributes = [attr.Name.encode("utf-8") for attr in response.results[0].Attributes]
+            attributes = [
+                attr.Name.encode("utf-8")
+                for attr in response.results[0].Attributes
+            ]
 
-        writer = csv.writer(sys.stdout, quoting=csv.QUOTE_ALL, lineterminator='\n')
+        writer = csv.writer(
+            sys.stdout, quoting=csv.QUOTE_ALL, lineterminator='\n')
         header = ["SubscriberID", "EmailAddress", "SubscriberKey"]
         header.extend(attributes)
         writer.writerow(header)
@@ -152,7 +170,6 @@ authenticationurl: https://auth.exacttargetapis.com/v1/requestToken?legacy=1""".
 
             writer.writerow(fields)
 
-
     def retrieve_triggeredsend(self, args):
         """
         retrive a triggered send with customer key.
@@ -162,14 +179,22 @@ authenticationurl: https://auth.exacttargetapis.com/v1/requestToken?legacy=1""".
         """
         getTS = ET_Client.ET_TriggeredSend()
         getTS.auth_stub = self.client
-        getTS.props = ["CustomerKey", "Name", "TriggeredSendStatus", "ObjectID"]
-        getTS.search_filter = {'Property' : 'CustomerKey','SimpleOperator' : 'equals','Value' : args.customer_key}
+        getTS.props = [
+            "CustomerKey",
+            "Name",
+            "TriggeredSendStatus",
+            "ObjectID"
+        ]
+        getTS.search_filter = {
+            'Property': 'CustomerKey',
+            'SimpleOperator': 'equals',
+            'Value': args.customer_key
+        }
         getResponse = getTS.get()
 
         for result in getResponse.results:
             return result.ObjectID
         return ""
-
 
     def retrieve_sentevent(self, args):
         """
@@ -181,11 +206,27 @@ authenticationurl: https://auth.exacttargetapis.com/v1/requestToken?legacy=1""".
         triggeredSendDefinitionObjectID = self.retrieve_triggeredsend(args)
         getSentEvent = ET_Client.ET_SentEvent()
         getSentEvent.auth_stub = self.client
-        getSentEvent.props = ["SendID","SubscriberKey","EventDate","Client.ID","EventType","BatchID","TriggeredSendDefinitionObjectID","ListID","PartnerKey","SubscriberID"]
-        getSentEvent.search_filter = {'Property' : 'TriggeredSendDefinitionObjectID','SimpleOperator' : 'equals','Value' : triggeredSendDefinitionObjectID}
+        getSentEvent.props = [
+            "SendID",
+            "SubscriberKey",
+            "EventDate",
+            "Client.ID",
+            "EventType",
+            "BatchID",
+            "TriggeredSendDefinitionObjectID",
+            "ListID",
+            "PartnerKey",
+            "SubscriberID"
+        ]
+        getSentEvent.search_filter = {
+            'Property': 'TriggeredSendDefinitionObjectID',
+            'SimpleOperator': 'equals',
+            'Value': triggeredSendDefinitionObjectID
+        }
         getResponse = getSentEvent.get()
 
-        writer = csv.writer(sys.stdout, quoting=csv.QUOTE_ALL, lineterminator='\n')
+        writer = csv.writer(
+            sys.stdout, quoting=csv.QUOTE_ALL, lineterminator='\n')
         writer.writerow(["EventDate", "SubscriberID"])
         for result in getResponse.results:
             writer.writerow([result.EventDate, result.SubscriberKey])
@@ -194,7 +235,6 @@ authenticationurl: https://auth.exacttargetapis.com/v1/requestToken?legacy=1""".
             getResponse = getSentEvent.getMoreResults()
             for result in getResponse.results:
                 writer.writerow([result.EventDate, result.SubscriberKey])
-
 
     def retrieve_openevent(self, args):
         """
@@ -206,11 +246,27 @@ authenticationurl: https://auth.exacttargetapis.com/v1/requestToken?legacy=1""".
         triggeredSendDefinitionObjectID = self.retrieve_triggeredsend(args)
         getSentEvent = ET_Client.ET_OpenEvent()
         getSentEvent.auth_stub = self.client
-        getSentEvent.props = ["SendID","SubscriberKey","EventDate","Client.ID","EventType","BatchID","TriggeredSendDefinitionObjectID","ListID","PartnerKey","SubscriberID"]
-        getSentEvent.search_filter = {'Property' : 'TriggeredSendDefinitionObjectID','SimpleOperator' : 'equals','Value' : triggeredSendDefinitionObjectID}
+        getSentEvent.props = [
+            "SendID",
+            "SubscriberKey",
+            "EventDate",
+            "Client.ID",
+            "EventType",
+            "BatchID",
+            "TriggeredSendDefinitionObjectID",
+            "ListID",
+            "PartnerKey",
+            "SubscriberID"
+        ]
+        getSentEvent.search_filter = {
+            'Property': 'TriggeredSendDefinitionObjectID',
+            'SimpleOperator': 'equals',
+            'Value': triggeredSendDefinitionObjectID
+        }
         getResponse = getSentEvent.get()
 
-        writer = csv.writer(sys.stdout, quoting=csv.QUOTE_ALL, lineterminator='\n')
+        writer = csv.writer(
+            sys.stdout, quoting=csv.QUOTE_ALL, lineterminator='\n')
         writer.writerow(["EventDate", "SubscriberID"])
         for result in getResponse.results:
             writer.writerow([result.EventDate, result.SubscriberKey])
@@ -219,7 +275,6 @@ authenticationurl: https://auth.exacttargetapis.com/v1/requestToken?legacy=1""".
             getResponse = getSentEvent.getMoreResults()
             for result in getResponse.results:
                 writer.writerow([result.EventDate, result.SubscriberKey])
-
 
     def retrieve_bounceevent(self, args):
         """
@@ -231,11 +286,27 @@ authenticationurl: https://auth.exacttargetapis.com/v1/requestToken?legacy=1""".
         triggeredSendDefinitionObjectID = self.retrieve_triggeredsend(args)
         getSentEvent = ET_Client.ET_BounceEvent()
         getSentEvent.auth_stub = self.client
-        getSentEvent.props = ["SendID","SubscriberKey","EventDate","Client.ID","EventType","BatchID","TriggeredSendDefinitionObjectID","ListID","PartnerKey","SubscriberID"]
-        getSentEvent.search_filter = {'Property' : 'TriggeredSendDefinitionObjectID','SimpleOperator' : 'equals','Value' : triggeredSendDefinitionObjectID}
+        getSentEvent.props = [
+            "SendID",
+            "SubscriberKey",
+            "EventDate",
+            "Client.ID",
+            "EventType",
+            "BatchID",
+            "TriggeredSendDefinitionObjectID",
+            "ListID",
+            "PartnerKey",
+            "SubscriberID"
+        ]
+        getSentEvent.search_filter = {
+            'Property': 'TriggeredSendDefinitionObjectID',
+            'SimpleOperator': 'equals',
+            'Value': triggeredSendDefinitionObjectID
+        }
         getResponse = getSentEvent.get()
 
-        writer = csv.writer(sys.stdout, quoting=csv.QUOTE_ALL, lineterminator='\n')
+        writer = csv.writer(
+            sys.stdout, quoting=csv.QUOTE_ALL, lineterminator='\n')
         writer.writerow(["EventDate", "SubscriberID"])
         for result in getResponse.results:
             writer.writerow([result.EventDate, result.SubscriberKey])
@@ -244,7 +315,6 @@ authenticationurl: https://auth.exacttargetapis.com/v1/requestToken?legacy=1""".
             getResponse = getSentEvent.getMoreResults()
             for result in getResponse.results:
                 writer.writerow([result.EventDate, result.SubscriberKey])
-
 
     def create_de_row(self, args):
         """
@@ -259,5 +329,4 @@ authenticationurl: https://auth.exacttargetapis.com/v1/requestToken?legacy=1""".
         de4.auth_stub = self.client
         de4.props = json.loads(args.attributes_json)
         postResponse = de4.post()
-        for result in postResponse.results:
-            print(result)
+        print(json.dumps(postResponse.results))
